e03377f32e828f25b7bb86b33211dbb1b656ff90
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2020-09-23 10:24:23 +0200

    Fixed routing issue.

--- client/src/App.js
+++ client/src/App.js
@@ -2,12 +2,24 @@ import React from 'react';
 
 import "./assets/sass/custom.scss";
 import "./assets/sass/timer.sass";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
+import Profile from "./components/Profile";
+import Game from "./components/Game";
 
 function App() {
     return (
-        <div className="App">
-
-        </div>
+        <BrowserRouter>
+            <div className="app">
+                <Switch>
+                    <Route exact path="/">
+                        <Profile />
+                    </Route>
+                    <Route path="/game">
+                        <Game />
+                    </Route>
+                </Switch>
+            </div>
+        </BrowserRouter>
     );
 }
 
