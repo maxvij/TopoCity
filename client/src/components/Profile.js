0b35105a325354d7d88f7c8b89eedb9620f21f4c
niklasmartin <niklasmartin@outlook.de>
2020-10-14 12:01:25 +0200

    Added new profile

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Button from "react-bootstrap/Button";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import {withRouter} from 'react-router-dom';
 
 class Profile extends React.Component {
     constructor(props) {
@@ -28,8 +29,8 @@ class Profile extends React.Component {
         localStorage.setItem('topo_name', name);
         localStorage.setItem('topo_user_id', id);
         localStorage.setItem('topo_origin', origin);
-        // console.log(profile)
-        // this.props.history.push('/province');
+        console.log(this.state.profile)
+        this.props.history.push('/province');
     }
 
     componentDidMount() {
@@ -45,17 +46,21 @@ class Profile extends React.Component {
     render() {
         return (
             <div className="center-box">
-                <h1>Hi, who are you?</h1>
+                <h1>Nice that you're here!</h1>
                 <div className="filler-40"></div>
                 <div className="max-400">
-                    <DropdownButton id="dropdown-basic-button" size='lg' title="Select an existing profile">
+                <div className="filler-40"></div>
+                    <h3>Have you played before?</h3>
+                    <DropdownButton variant="blue" size='lg' title="Select an existing profile">
                         {this.state.profiles.map((profile, index) => (
                             <Dropdown.Item
-                                onClick={() => this.handleSelect(profile.id, profile.name, profile.origin)}>{profile.name}</Dropdown.Item>
+                                onClick={() => this.handleSelect(profile.id, profile.name, profile.homes)}>{profile.name}</Dropdown.Item>
                         ))}
                     </DropdownButton>
+                    <div className="filler-40"></div>
+                    <h3>Are you new here?</h3>
                     <Button href="/name" variant="yellow" size="lg" color="blue" block>
-                        New Learner
+                        Create a new profile
                     </Button>
                 </div>
             </div>
@@ -64,4 +69,4 @@ class Profile extends React.Component {
     }
 }
 
-export default Profile
+export default withRouter(Profile)
