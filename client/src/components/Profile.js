d22744ac55bad1dcf4855807f6f183fac7a229b5
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2020-10-14 12:10:57 +0200

    Added profile selection (#34)
    * Changes
    
    * Fixed Profile map function.
    
    * Added new profile
    
    Co-authored-by: niklasmartin <niklasmartin@outlook.de>

--- client/src/components/Profile.js
+++ client/src/components/Profile.js
@@ -1,26 +1,67 @@
 import React from 'react'
 import Button from "react-bootstrap/Button";
+import Dropdown from "react-bootstrap/Dropdown";
+import DropdownButton from "react-bootstrap/DropdownButton";
+import {withRouter} from 'react-router-dom';
 
 class Profile extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSelect = this.handleSelect.bind(this);
+        this.state = {
+            profiles: [],
+            profile: {
+                name: '',
+                id: 0,
+                origin: ''
+            }
+        }
+    }
+
+    handleSelect(id, name, origin) {
+        this.setState({
+            profile: {
+                name: name,
+                id: id,
+                origin: origin
+            }
+        })
+        localStorage.setItem('topo_name', name);
+        localStorage.setItem('topo_user_id', id);
+        localStorage.setItem('topo_origin', origin);
+        console.log(this.state.profile)
+        this.props.history.push('/province');
+    }
+
+    componentDidMount() {
+        fetch('/users')
+            .then(res => res.json())
+            .then((res) => {
+                this.setState({profiles: res})
+                console.log(res)
+            })
+            .catch(console.log)
+    }
+
     render() {
         return (
             <div className="center-box">
-                <h1>Hi, who are you?</h1>
+                <h1>Nice that you're here!</h1>
                 <div className="filler-40"></div>
                 <div className="max-400">
-                    <Button variant="blue" size="lg" color="blue" block>
-                        Nick
-                    </Button>
-                    <Button variant="blue" size="lg" color="blue" block>
-                        Max
-                    </Button>
-                    <Button variant="blue" size="lg" color="blue" block>
-                        Karlijn
-                    </Button>
+                <div className="filler-40"></div>
+                    <h3>Have you played before?</h3>
+                    <DropdownButton variant="blue" size='lg' title="Select an existing profile">
+                        {this.state.profiles.map((profile, index) => (
+                            <Dropdown.Item
+                                onClick={() => this.handleSelect(profile.id, profile.name, profile.homes)}>{profile.name}</Dropdown.Item>
+                        ))}
+                    </DropdownButton>
+                    <div className="filler-40"></div>
+                    <h3>Are you new here?</h3>
                     <Button href="/name" variant="yellow" size="lg" color="blue" block>
-                        New Learner
+                        Create a new profile
                     </Button>
-
                 </div>
             </div>
 
@@ -28,4 +69,4 @@ class Profile extends React.Component {
     }
 }
 
-export default Profile
+export default withRouter(Profile)
