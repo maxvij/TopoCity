6a9ce985ef77a48cb367f0f167bf09cd8864194d
Max van IJsselmuiden <maxvanijsselmuiden@gmail.com>
2020-10-17 12:10:36 +0200

    Fixed this.props.loading for intro buttons.

--- client/src/components/game/GameIntro.js
+++ client/src/components/game/GameIntro.js
@@ -7,7 +7,7 @@ export default class GameIntro extends React.Component {
             <div className="max-400">
                 <h3>Ready for the test?</h3>
                 <p>We will now start the testing phase. <br /> The testing session will take 10 minutes. <br /> Are you ready?</p>
-                <Button variant="green" size="lg" color="blue" block disabled={!this.props.loading} onClick={this.props.startGame}>
+                <Button variant="green" size="lg" color="blue" block disabled={this.props.loading} onClick={this.props.startGame}>
                     {this.props.loading ? "..." : "Start testing!"}
                 </Button>
             </div>
